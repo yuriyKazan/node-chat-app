f6c1cea06bd06f2aaa8ac783dbe38597ce5d3d0a
yuriy.kazan <yuriy_kazan@edvantis.com>
2017-03-22 15:18:52 +0200

    Initial commit.

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+/*
+app.get('/', (req, res, next) => {
+  res.status(200).sendFile('index.html', (err) => {
+      if (err) next(err);
+  });
+});
+*/
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
